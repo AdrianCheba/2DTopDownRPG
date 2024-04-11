a8d2f726cbf30bca65ad68f2dd9af9ebb944b1b1
Adrian Cheba <adiche@wp.pl>
2024-04-11 21:36:22 +0200

    Added Player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,11 +11,15 @@ public class PlayerController : MonoBehaviour
     PlayerControls _playerControls;
     Vector2 _movement;
     Rigidbody2D _rbody;
+    Animator _playerAnimator;
+    SpriteRenderer _playerSpriteRenderer;
 
     private void Awake()
     {
         _rbody = GetComponent<Rigidbody2D>();
         _playerControls = new PlayerControls();
+        _playerAnimator = GetComponent<Animator>();
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void OnEnable()
     {
@@ -29,16 +33,31 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     void PlayerInput()
     {
         _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+
+        _playerAnimator.SetFloat("moveX", _movement.x);
+        _playerAnimator.SetFloat("moveY", _movement.y);
     }
 
     void Move()
     {
         _rbody.MovePosition(_rbody.position + _movement * (_moveSpeed * Time.fixedDeltaTime));
+    }
+
+    void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if(mousePos.x < playerScreenPoint.x)
+            _playerSpriteRenderer.flipX = true;
+        else
+            _playerSpriteRenderer.flipX = false;
     }
 }
